ef04086229637b5172a53477ec7daf551264cd74
Ankush073 <ankush.ankush2414@gmail.com>
2024-05-23 18:40:29 +0530

    commit all files

--- src/index.js
+++ src/index.js
@@ -4,3 +4,11 @@ connectDB()
 dotenv.config({
       path:'./env'
 })
+
+.then(()=>{
+      app.listen(process.env.port||800,()=>{
+            console.log(`server listening on port:${process.env.port}`);
+      })})
+.catch((error)=>{
+      console.log("MongoDB connection faliled",error);
+})
