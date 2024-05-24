657c87810f49a7db7178bdb3dd095bf1cdaba13f
Ankush073 <ankush.ankush2414@gmail.com>
2024-05-24 20:03:34 +0530

    commit all files

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -1,4 +1,8 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import {uploadOnCloudinary} from "../utils/clodinary.js";
+import User from "..models/users.models.user.js";
+import {ApiError} from "..utils/ApiError.js";
+import {ApiResponse} from "..utils/ApiResponse.js";
 const registerUser =asyncHandler(async(req,res)=>
       {
             // 1.Get user name and password from frontend 
@@ -10,7 +14,48 @@ const registerUser =asyncHandler(async(req,res)=>
             //7.remove password and refresh token from response 
             //8.check for user creation 
             //9.return response 
+            const {fullname ,email,password,username}=req.body;
+            console.log("Email:",email)
 
+           if(
+            [email,fullname,password,username].some((fields)=>
+                  fields?.trim()==="")
+            )
+           {
+            throw new ApiError(400,"All field are required")
+           }
 
+           const Existinguser=User.findone({
+            $or:[{username},{email}]
+           })
+           if(Existinguser){
+            throw new ApiError(409,"Already user exists")
+           }
+           const avtarlocalpath=req.files?.avtar[0]?.path
+           const CoverImagelocalpath=req.files?.avtar[0]?.path
+           const avtar=await uploadOnCloudinary(avtarlocalpath)
+           const coverImage= await uploadOnCloudinary(CoverImagelocalpath)
+           if(!avtar){
+            throw new ApiError(409,"Avtar is required")
+           }
+
+           const user = await User.create(
+            {
+                  fullname,
+                  avtar:avtar.url,
+                  coverImage:coverImage?.url|| "",
+                  password,
+                  email,
+                  username:username.toLowerCase()
+            })
+            const createUser= User.findById(user.id).select(
+                  "-password -refreshToken"
+            )
+            if(!createUser){
+                  throw new ApiError(500,"Something went wrong while registering the user")
+            }
+            return res.status(201).json(
+                  new ApiResponse(200,createUser,"User Registred succesfully")
+            )
       })
 export {registerUser}
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,5 +1,21 @@
 import {Router} from 'express'
 import {registerUser} from '../controllers/users.controller.js';
+import {upload} from "../middlewares/multer.middlewares.js"
+
 const router =Router()
-router.route("/register").post(registerUser)
+router.route("/register").post(
+      upload.fields(
+            [
+                  {
+                        name:"Avtar ",
+                        maxCount:1
+                  },
+            
+                  {
+                        name:"CoverImage",
+                        maxCount:1
+                  }
+            ]
+      ),
+      registerUser)
 export default router 
